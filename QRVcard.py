cfa1bd784a3563fa75a96c4405f4f98f106cc8d7
PHIL KIM <philsgu@gmail.com>
2022-12-03 13:34:37 -0800

    qr make v1

--- QRVcard.py
+++ QRVcard.py
@@ -1,9 +1,15 @@
 import streamlit as st
 import re 
+import segno
+from segno import helpers
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import io
 
 #create a session state for QR image to remain upon download
 if "submitted" not in st.session_state:
-    st.session_state.submitted = False
+    st.session_state['submitted'] = False
 
 #Regex to catch validation errors
 phone_format = r"^[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4}$"
@@ -24,10 +30,47 @@ st.write('This will produce a QR code in VCard format')
 #title
 profession_degree = ["DO", "MPH", "MD", "PhD"]
 
+#create QR Function formation
+def vcardmake (displayname, email=None, title=None, org=None, phone=None, workphone=None, fax=None, street=None, city=None, region=None, zipcode=None, url=None):
+    n  = displayname.split(' ')
+    n.append(';')
+    n.insert(2, ' ')
+    name = ''.join(n[1:]) + n[0]
+    # use a bitmap font
+    #font = ImageFont.truetype("arial.ttf", 12)
+    vcard = helpers.make_vcard(
+        name=name,
+        displayname=displayname,
+        email=email,
+        title=title,
+        org=org,
+        phone=phone,
+        workphone=workphone,
+        fax=fax,
+        street=street,
+        city=city,
+        region=region,
+        zipcode=zipcode,
+        url=url
+    )
+    out = io.BytesIO()
+    vcard.save(out, scale=2, border=20, kind='png') #save to buffer
+    out.seek(0)
+    
+    img = Image.open(out)
+    #d1 = ImageDraw.Draw(img)
+    #myfont = ImageFont.truetype("/System/Library/Fonts/Geneva.ttf", 12)
+    #img_width, img_height = img.size
+    #d1.text((img_width//2, 180), displayname, anchor='ms')
+    
+    #img.save(f"/Users/pkimmd/Documents/GitHub/qrvcard/QR_images/{displayname}.png")
+    return st.image(img, use_column_width='auto')
 
-#better error check
+
+##################################################
 def check_valid():
     st.session_state.submitted = True
+   
     for k, v in st.session_state.items():
         if k == 'Address':
             if st.session_state[k]:
@@ -96,23 +139,29 @@ def check_valid():
                     st.session_state.submitted = False
                     st.error(f"Incorrect Number Format: {k}")
         if k == 'Work':
-              if st.session_state[k]:
+            if st.session_state[k]:
                 if not re.match(phone_format, st.session_state[k]):
                     st.session_state.submitted = False
                     st.error(f"Incorrect Number Format: {k}")
         if k == 'Fax':
-              if st.session_state[k]:
+            if st.session_state[k]:
                 if not re.match(phone_format, st.session_state[k]):
                     st.session_state.submitted = False
-                    st.error(f"Incorrect Number Format: {k}")
-        
+                    st.error(f"Incorrect Number Format: {k}") 
 
     if not st.session_state['First']:
         st.session_state.submitted = False
         st.error(f"Missing Required Value: First")
+    elif not re.match(r'^[a-zA-Z\s]+$', st.session_state['First'].strip()):
+        st.session_state.submitted = False
+        st.error(f"No numerics allowed: First")
+        
     if not st.session_state['Last']:
         st.session_state.submitted = False
-        st.error(f"Missing Required Value: Last")        
+        st.error(f"Missing Required Value: Last")
+    elif not re.match(r'^[a-zA-Z\s]+$', st.session_state['Last'].strip()):
+        st.session_state.submitted = False
+        st.error(f"No numerics allowed: Last")        
             
     if not st.session_state['Email']:
         st.session_state.submitted = False
@@ -125,82 +174,26 @@ def check_valid():
     if st.session_state['Website']:
         if not re.match(url_format, st.session_state['Website']):
             st.session_state.submitted = False
-            st.error(f"Incorrect URL Format")
-            
-
-
-
- 
-    
-    
-
-        
-    
-                
-
-
-#form Error check function
-def check_error():
-    st.session_state.submitted = True
-    mykeylist = ['First', 'Last']
-    numbers = ['Cell', 'Work', 'Work_Fax']
-    address = ['Address', 'City', 'Zip']
-    for mykey in mykeylist:
-        if not st.session_state[mykey]:
-            st.session_state.submitted = False
-            st.error(f"Missing Value: {mykey}")
-        elif not re.match(r'^[a-zA-Z\s]+$', st.session_state[mykey].strip()):
-            st.session_state.submitted = False
-            st.error(f"No numerics allowed: {mykey}")
-        
-    for number in numbers:
-        if not re.match(phone_format, st.session_state[number]):
-            st.session_state.submitted = False
-            st.error(f"Incorrect Number Format: {number}")
-    if st.session_state.Address or st.session_state.City or st.session_state.State or st.session_state.Zip:
-        for a in address:
-            if not st.session_state[a]:
-                st.session_state.submitted = False
-                st.error(f"Missing Address Field: {a}")
-    # if not re.match(zip_format, st.session_state['Zip']):
-    #     st.session_state.submitted = False
-    #     st.error(f"Incorrect Format: Zip")       
-
-    if not st.session_state.Email:
-        st.session_state.submitted = False
-        st.error("Missing Email")
-    elif not re.match(email_format, st.session_state.Email):
-        st.session_state.submitted = False
-        st.error("Incorrect email address format")
-
-    if st.session_state.Website:
-        if not re.match(url_format, st.session_state.Website): 
-            st.session_state.submitted = False
-            st.error("Incorrect URL Format")
-
-    
-    
-        
-                
-
+            st.error(f"Incorrect URL Format")                            
+###############################################            
 with st.form('contact_info'):
-    
     st.markdown("###### *Denotes Required")
     col1, bra, col2 = st.columns([3,1,3])
+
     with col1:
         first_name = st.text_input("First*", key="First").capitalize()
     with col2:
         last_name = st.text_input("Last*", key="Last").capitalize()
     
-    degree = st.multiselect("Professional Degree(s)", options=profession_degree)
-    title = st.text_input("Enter Occupation Title")
-    org = st.text_input("Organization")
+    degree = st.multiselect("Professional Degree(s)", options=profession_degree, key='Degree')
+    title = st.text_input("Enter Occupation Title", key='Title')
+    org = st.text_input("Organization", key='Organization')
     personal_cell = st.text_input("Cell Number", key='Cell', help='US Number Format (xxx)xxx-xxxxx')
     work_phone = st.text_input("Work Number", key='Work')
     work_fax = st.text_input("Work Fax", key='Fax')
-
     business_add = st.text_input("Business Address", key='Address')
     add1, add2, add3 = st.columns([4, 2, 4])
+
     with add1:
         city = st.text_input("City", key='City')
     with add2:
@@ -210,15 +203,20 @@ with st.form('contact_info'):
 
     email = st.text_input("Email*", key='Email')
     website = st.text_input("Website", key='Website')
-
-
     submit = st.form_submit_button("Create VCard", on_click=check_valid)
 
+    #execute code
     if st.session_state.submitted and submit:
         st.write('valid')
-
         st.write(first_name, last_name, ", ".join(degree))
+        st.write(title, org )
+        displayname = str()
+        if degree:
+            displayname = first_name + last_name + ' ,' + degree
+        else:
+            displayname = (' ').join([first_name, last_name])
         
+        vcardmake(displayname=displayname, email=email, title=title, org=org, phone=personal_cell, workphone=work_phone, fax=work_fax, street=business_add, city=city, region=state, zipcode=zip_code, url=website)
 
 ##Sample Write output          
 st.write (st.session_state["First"])
